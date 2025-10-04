4251a9410a4e28db0683605a9acec84850915c59
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-04 14:25:48 +0530

    Added popups file

---
+++ elements/popups.lua
@@ -0,0 +1,15 @@
+local popups = {}
+
+function popups:load()
+
+end
+
+function popups:update(dt)
+
+end
+
+function popups:draw()
+
+end
+
+return popups
--- main.lua
+++ main.lua
@@ -1,12 +1,15 @@
-function love.load()
+local popups = require "elements.popups"
 
+
+function love.load()
+    popups:load()
 end
 
-function love.update()
-
+function love.update(dt)
+    popups:update(dt)
 end
 
 function love.draw()
-
+    popups:draw()
 end
 
