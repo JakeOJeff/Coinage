b4aac0a2e7aba958e3df7928201e41ef8c8c6228
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-04 22:47:14 +0530

    Added working velocity for the ball

--- elements/coin.lua
+++ elements/coin.lua
@@ -7,7 +7,23 @@ function coin:load(world)
     self.yVel = 0
     self.img = love.graphics.newImage("assets/ball.png")
  
-    self.body = love.physics.newBody(world, self.width/2)
+    self.body = love.physics.newBody(world, 100, 800, "dynamic")
+    self.shape = love.physics.newCircleShape(60)
+    self.fixture = love.physics.newFixture(self.body, self.shape)
+
+    self.fixture:setRestitution(0.5)
+end
+
+function coin:update(dt)
+    self.x, self.y = self.body:getPosition()
+    self.xVel, self.yVel = self.body:getLinearVelocity()
+
+end
+
+function game:draw()
+
+    
+
 end
 
 return coin
