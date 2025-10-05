942f61baaf7e3ff4b831ecac1e9042aca5370fee
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-06 04:28:15 +0530

    Added a add item feature

--- elements/popups.lua
+++ elements/popups.lua
@@ -8,7 +8,28 @@ function popups:load()
     wW = love.graphics.getWidth()
     wH = love.graphics.getHeight()
     self.enabled = false
-    self.slabs = { }
+    self.itemList = {
+        {
+            name = "Coins",
+            quantity = 1,
+            img = love.graphics.newImage("assets/coin.png"),
+            func = function()
+                table.insert(Coins, COIN:new(200, 200))
+            end,
+            rarity = 1 / 2
+        },
+        {
+            name = "Orpheus",
+            quantity = 1,
+            img = love.graphics.newImage("assets/orpheus.png"),
+            func = function()
+                
+            end,
+            rarity = 1 / 4
+        },
+    }
+    self.itemInventory = {}
+    self.slabs = {}
     --[[
     {
         name = "Coins",
@@ -22,6 +43,28 @@ end
 
 function popups:update(dt)
 
+end
+
+function popups:addItems()
+    local chosenItem = pickRandomItem(self.itemList)
+    chosenItem.quantity = love.math.random(1, 3)
+    table.insert(self.slabs, chosenItem)
+end
+function pickRandomItem(itemList)
+    local totalWeight = 0
+    for _, item in ipairs(itemList) do
+        totalWeight = totalWeight + item.rarity
+    end
+
+    local random = math.random() * totalWeight
+    local current = 0
+
+    for _, item in ipairs(itemList) do
+        current = current + item.rarity
+        if random <= current then
+            return item
+        end
+    end
 end
 
 function popups:draw()
@@ -43,22 +86,22 @@ function popups:draw()
             love.graphics.rectangle("line", x, wH / 2 - w / 2, w, w, 25, 25)
             local img = self.slabs[i].img
             local imgW, imgH = img:getWidth(), img:getHeight()
-            local scale = (w * 0.7) / imgW 
+            local scale = (w * 0.7) / imgW
 
             local y = 220
 
             love.graphics.draw(
                 img,
-                x + w / 2, 
-                y + w / 2, 
-                0, 
-                scale, scale, 
-                imgW / 2, imgH / 2 
+                x + w / 2,
+                y + w / 2,
+                0,
+                scale, scale,
+                imgW / 2, imgH / 2
             )
-                    love.graphics.setFont(itemFont)
-        love.graphics.print(self.slabs[i].name.." x"..self.slabs[i].quantity, x + w / 2 - itemFont:getWidth(self.slabs[i].name.." x"..self.slabs[i].quantity)/2, y + w/2 + 100)
+            love.graphics.setFont(itemFont)
+            love.graphics.print(self.slabs[i].name .. " x" .. self.slabs[i].quantity,
+                x + w / 2 - itemFont:getWidth(self.slabs[i].name .. " x" .. self.slabs[i].quantity) / 2, y + w / 2 + 100)
         end
-
     end
     love.graphics.setColor(1, 1, 1)
 end
