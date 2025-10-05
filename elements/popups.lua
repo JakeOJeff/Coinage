cf99df8480552585de46740206b62d28ac4e0817
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-06 03:05:55 +0530

    Added rectangular slabs and systems

--- elements/popups.lua
+++ elements/popups.lua
@@ -7,6 +7,7 @@ function popups:load()
     wW = love.graphics.getWidth()
     wH = love.graphics.getHeight()
     self.enabled = true
+    self.slabs = { {}, {} }
 end
 
 function popups:update(dt)
@@ -21,6 +22,16 @@ function popups:draw()
         love.graphics.setFont(headerFont)
         love.graphics.setColor(1, 1, 1, 0.7)
         love.graphics.print("YOU HAVE EARNED", 50 + (wW - 100) / 2 - headerFont:getWidth("YOU HAVE EARNED") / 2, 100)
+
+        local w = 250
+        local spacing = 10
+        local totalWidth = #self.slabs * (w + spacing) - spacing
+        local startX = (wW / 2) - (totalWidth / 2)
+
+        for i = 1, #self.slabs do
+            local x = startX + (i - 1) * (w + spacing)
+            love.graphics.rectangle("line", x, wH/2 - w/2, w, w, 25, 25)
+        end
     end
     love.graphics.setColor(1, 1, 1)
 end
